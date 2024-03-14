3040746e4c30089319b9eb2c4e815f75054ea498
milenatavares <milenatavares@lampp-it.com.br>
2024-03-14 14:53:43 -0300

    MODULO 02 - AULA 08 (Finalizada)

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,10 +1,18 @@
 <h1>Cadastrar novo post</h1>
 
+@if($errors->any())
+    <ul>
+    @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+    @endforeach
+    </ul>
+@endif
+
 <form action="{{ route('posts.store') }}" method="POST">
     @csrf
 
-    <input type="text" name="title" id="title">
-    <textarea name="content" id="content" cols="30" rows="4"></textarea>
+    <input type="text" name="title" id="title" value="{{ old('title') }}">
+    <textarea name="content" id="content" cols="30" rows="4">{{ old('content') }}</textarea>
     
     <button type="submit">Enviar</button>
 
